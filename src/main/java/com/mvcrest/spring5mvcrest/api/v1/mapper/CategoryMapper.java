8583dd466d6f17a185dfbd77e2866de463c17dfb
bachardaowd <bachar.daowd@gmail.com>
2022-12-06 14:37:35 +0100

    configuration testing

--- src/main/java/com/mvcrest/spring5mvcrest/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/mvcrest/spring5mvcrest/api/v1/mapper/CategoryMapper.java
@@ -1,8 +1,12 @@
 package com.mvcrest.spring5mvcrest.api.v1.mapper;
 
+import com.mvcrest.spring5mvcrest.api.v1.model.CategoryDTO;
+import com.mvcrest.spring5mvcrest.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryMapper {
-
+CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+CategoryDTO categoryToCategoryDTO(Category category);
 }
