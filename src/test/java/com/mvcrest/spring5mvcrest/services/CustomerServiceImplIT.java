27f7ea474b2b5b5cdbdc5cd86a75b764e71fc2ea
bachardaowd <bachar.daowd@gmail.com>
2022-12-07 18:29:29 +0100

    Running Integration test with Patch customer service

---
+++ src/test/java/com/mvcrest/spring5mvcrest/services/CustomerServiceImplIT.java
@@ -0,0 +1,106 @@
+package com.mvcrest.spring5mvcrest.services;
+
+
+import com.mvcrest.spring5mvcrest.api.v1.mapper.CustomerMapper;
+import com.mvcrest.spring5mvcrest.api.v1.model.CustomerDTO;
+import com.mvcrest.spring5mvcrest.bootstrap.Bootstrap;
+import com.mvcrest.spring5mvcrest.domain.Customer;
+import com.mvcrest.spring5mvcrest.repositories.CategoryRepository;
+import com.mvcrest.spring5mvcrest.repositories.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class CustomerServiceImplIT {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    CustomerService customerService;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        System.out.println("Loading Customer Data");
+        System.out.println(customerRepository.findAll().size());
+
+        //Setup data for testing
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run();//load data
+
+        customerService = new CustomerServiceImpl(customerRepository, CustomerMapper.INSTANCE);
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstName() {
+        String updatedName = "UpdatedName";
+        Long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.findById(id).get();
+        assertNotNull(originalCustomer);
+
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstName());
+
+        assertThat(originalFirstName, not(equalTo(updatedCustomer.getFirstName())));
+        assertThat(originalLastName, (equalTo(updatedCustomer.getLastName())));
+    }
+
+    @Test
+    public void patchCustomerUpdateLastName() {
+        String updatedName = "UpdatedName";
+        Long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.findById(id).get();
+        assertNotNull(originalCustomer);
+
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstName();
+        String originalLastName = originalCustomer.getLastName();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastName(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastName());
+
+        assertThat(originalFirstName, equalTo(updatedCustomer.getFirstName()));
+        assertThat(originalLastName, not(equalTo(updatedCustomer.getLastName())));
+    }
+
+    private Long getCustomerIdValue() {
+        List<Customer> customers = customerRepository.findAll();
+        System.out.println("Customers Found: " + customers.size());
+
+        //Return first id
+        return customers.get(0).getId();
+    }
+}
